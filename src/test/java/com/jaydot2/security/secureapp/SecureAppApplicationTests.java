87d331fd803682f9f10914d2c1003bf6935f5341
James Bray <jrbrayjr@gmail.com>
2023-12-30 21:00:00 -0600

    Updating to Spring 3.2.1

--- src/test/java/com/jaydot2/security/secureapp/SecureAppApplicationTests.java
+++ src/test/java/com/jaydot2/security/secureapp/SecureAppApplicationTests.java
@@ -1,12 +1,12 @@
 package com.jaydot2.security.secureapp;
 
 
-import com.jaydot2.security.secureapp.config.ServiceSecurityConfiguration;
+import com.jaydot2.security.secureapp.config.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-@ContextConfiguration( classes = ServiceSecurityConfiguration.class )
+@ContextConfiguration( classes = SecurityConfig.class )
 @SpringBootTest
 public class SecureAppApplicationTests {
 
